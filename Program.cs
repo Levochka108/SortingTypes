0cceef5e38549083e6f30ab31d2791a60dbb9402
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-02-03 21:34:40 +0300

    Работа с классами в C#

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
-﻿void SortSelection(int[] collection)
-{
-    int size = collection.Length;
-
-    for (int i = 0; i < size - 1; i++)
-    {
-        int pos = i;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (collection[j] < collection[pos]) pos = j;
-        }
-        int temp = collection[i];
-        collection[i] = collection[pos];
-        collection[pos] = temp;
-    }
-}
+﻿using static Sorting;
 
 int[] array = { 7, 8, 5, 4, 3, 6, 2, 1, 0 };
+
 Console.WriteLine($"[{String.Join(',', array)}]");
 SortSelection(array);
+Console.WriteLine($"[{String.Join(',', array)}]");
