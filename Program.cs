b7759dfed431638521fd102b3eff4064c3b1b4a5
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-02-03 21:47:58 +0300

    Создал отдельный класс на печать массива

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using static Sorting;
 using static System.Console;
+using static System.String;
 
 int[] array = { 7, 8, 5, 4, 3, 6, 2, 1, 0 };
 
-WriteLine($"[{String.Join(',', array)}]");
 SortSelection(array);
-WriteLine($"[{String.Join(',', array)}]");
+WriteLine($"[{Join(',', array)}]");
