9f088d42000fbdfa24cc46abc4df147f39befa42
dmitrii klochkov <d.klochkov9421@gmail.com>
2023-02-03 21:38:21 +0300

    Работа с using static in  C#

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using static Sorting;
+using static System.Console;
 
 int[] array = { 7, 8, 5, 4, 3, 6, 2, 1, 0 };
 
-Console.WriteLine($"[{String.Join(',', array)}]");
+WriteLine($"[{String.Join(',', array)}]");
 SortSelection(array);
-Console.WriteLine($"[{String.Join(',', array)}]");
+WriteLine($"[{String.Join(',', array)}]");
